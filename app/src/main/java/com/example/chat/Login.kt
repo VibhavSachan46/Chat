eacfe95b84547580dff652350f352aa07d3be2f6
Vibhav <vibhavsachan743@gmail.com>
2023-01-04 23:13:36 +0530

    1:35 signup, login, main activity, message box.

--- app/src/main/java/com/example/chat/Login.kt
+++ app/src/main/java/com/example/chat/Login.kt
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
 import android.widget.EditText
+import android.widget.Toast
 import com.example.chat.R.*
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.ktx.Firebase
@@ -20,6 +21,8 @@ class Login : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(layout.activity_login)
+
+        supportActionBar?.hide()
 
         mAuth = FirebaseAuth.getInstance()
 
@@ -43,6 +46,18 @@ class Login : AppCompatActivity() {
     }
 
     private fun login(email: String, password: String){
-        //logic for creating user
+        //logic for logging user
+
+        mAuth.signInWithEmailAndPassword(email, password)
+            .addOnCompleteListener(this) { task ->
+                if (task.isSuccessful) {
+                    val intent = Intent(this@Login, MainActivity::class.java)
+                    finish()
+                    startActivity(intent)
+                } else {
+                    Toast.makeText(this@Login,"User does not exist",Toast.LENGTH_SHORT).show()
+                }
+            }
+
     }
 }
