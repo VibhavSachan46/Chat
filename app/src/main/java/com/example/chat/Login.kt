102179cbb9cd1de7001f604cfae8950c2044fd02
Vibhav <vibhavsachan743@gmail.com>
2023-01-04 19:22:29 +0530

    Second Commit

--- app/src/main/java/com/example/chat/Login.kt
+++ app/src/main/java/com/example/chat/Login.kt
@@ -1,11 +1,48 @@
 package com.example.chat
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import com.example.chat.R.*
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.ktx.Firebase
 
 class Login : AppCompatActivity() {
+
+    private lateinit var edtEmail: EditText
+    private lateinit var edtPassword: EditText
+    private lateinit var btnLogin: Button
+    private lateinit var btnSignUp: Button
+    private lateinit var mAuth: FirebaseAuth
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_login)
+        setContentView(layout.activity_login)
+
+        mAuth = FirebaseAuth.getInstance()
+
+        edtEmail = findViewById(R.id.edt_email)
+        edtPassword = findViewById(R.id.edt_password)
+        btnLogin = findViewById(R.id.btnLogin)
+        btnSignUp = findViewById(R.id.btnSignUp)
+
+        btnSignUp.setOnClickListener {
+            val intent = Intent(this , Signup::class.java)
+            startActivity(intent)
+        }
+
+        btnLogin.setOnClickListener {
+            val email = edtEmail.text.toString()
+            val password = edtPassword.text.toString()
+
+            login(email , password);
+        }
+
+    }
+
+    private fun login(email: String, password: String){
+        //logic for creating user
     }
 }
--- app/src/main/java/com/example/chat/Signup.kt
+++ app/src/main/java/com/example/chat/Signup.kt
@@ -2,10 +2,38 @@ package com.example.chat
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import com.example.chat.R.*
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseAuthWeakPasswordException
 
 class Signup : AppCompatActivity() {
+
+    private lateinit var edtName: EditText
+    private lateinit var edtEmail: EditText
+    private lateinit var edtPassword: EditText
+    private lateinit var btnSignUp: Button
+    private lateinit var mAuth: FirebaseAuth
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_signup)
+
+        edtName = findViewById(R.id.edt_name)
+        edtEmail = findViewById(R.id.edt_email)
+        edtPassword = findViewById(R.id.edt_password)
+        btnSignUp = findViewById(R.id.btnSignUp)
+
+        btnSignUp.setOnClickListener {
+            val email = edtEmail.text.toString()
+            val password = edtPassword.text.toString()
+
+            signUp(email ,password)
+        }
+    }
+
+    private fun signUp(email: String , password: String){
+        //logic for creating user
     }
 }
